783d053ffebb96a986870cbd0f4a3e6630f91d63
lynn <zl8312070@sina.com>
2020-10-12 09:15:11 +0800

    feat: 引入session

--- server/middlewares/common.js
+++ server/middlewares/common.js
@@ -5,6 +5,7 @@
  */
 import bodyParser from 'koa-bodyparser'
 import logger from 'koa-logger'
+import session from 'koa-session'
 
 export const addBodyParser = app => {
     app.use(bodyParser())
@@ -12,4 +13,19 @@ export const addBodyParser = app => {
 
 export const addLogger = app => {
     app.use(logger())
+}
+
+export const addSession = app => {
+    app.keys = ['lynn-trailer']
+
+    const CONFIG = {
+        key: 'koa: sess',
+        maxAge: 86400000,
+        overwrite: true,
+        httpOnly: false,
+        signed: true,
+        rolling: false
+    }
+
+    app.use(session((CONFIG, app)))
 }
