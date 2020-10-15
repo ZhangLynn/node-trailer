6ab2e816955d124ba8ec34b8b5427d1544c20a83
lynn <zl8312070@sina.com>
2020-10-15 22:23:10 +0800

    chore: pm2配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    script: './server/index.js',
+    watch: '.',
+    name: 'trailer',
+    env: {
+      COMMON_VARIABLE: 'true'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+  deploy : {
+    production : {
+      user : 'root',
+      host : '122.51.186.171',
+      port: '22',
+      ref  : 'origin/master',
+      repo : 'https://github.com/ZhangLynn/node-trailer',
+      ssh_options: 'StrictHostKeyChecking=no',
+      path : '/www/node-trailer/production',
+      'pre-deploy-local': 'git fetch --all',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+};
