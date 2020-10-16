c39fb96267281b09b07a8d132562d78de55605de
lynn <zl8312070@sina.com>
2020-10-16 10:48:40 +0800

    feat: 配置babel es2015

--- start.js
+++ start.js
@@ -3,6 +3,8 @@
  * @date: 2020/10/6
  * @description:
  */
-require("babel-core/register")()
+require("babel-core/register")({
+    presets: ['es2015', 'stage-0']
+})
 require('babel-polyfill')
 require('./server/index.js')
